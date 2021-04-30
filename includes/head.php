65ce7b315b2bc4eeb4db5179ea189003f2bbc145
velikhanov <teymurvk@mail.ru>
2021-04-30 18:09:16 +0400

    Update

--- includes/head.php
+++ includes/head.php
@@ -2,7 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="shortcut icon" href="img/e.png" type="image/x-icon">
-<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&family=Roboto+Slab:wght@900&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="css/bootstrap.min.css">
 <link rel="stylesheet" href="css/all.min.css">
 <link rel="stylesheet" href="css/main.css">
