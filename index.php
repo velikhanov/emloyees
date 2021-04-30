3931095f8c8c4d9d34142d525f8d46998744dbb9
velikhanov <teymurvk@mail.ru>
2021-04-30 17:45:33 +0400

    Update

--- index.php
+++ index.php
@@ -4,138 +4,42 @@
 <html lang="en">
 <head>
   <?php require "includes/head.php" ?>
+  <link rel="stylesheet" href="css/index.css">
   <title>Employees</title>
 </head>
 <body style="background-color: #fff;">
 
   <?php require "includes/header.php" ?>
-  <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-  <symbol id="check" viewBox="0 0 16 16">
-    <title>Check</title>
-    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"></path>
-  </symbol>
-</svg>
-  <div class="container py-3">
-
-
-<div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-  <h1 class="display-4 fw-normal">Pricing</h1>
-  <p class="fs-5 text-muted">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It’s built with default Bootstrap components and utilities with little customization.</p>
-</div>
-
-<main>
-  <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-    <div class="col">
-      <div class="card mb-4 rounded-3 shadow-sm">
-        <div class="card-header py-3">
-          <h4 class="my-0 fw-normal">Free</h4>
-        </div>
-        <div class="card-body">
-          <h1 class="card-title pricing-card-title">$0<small class="text-muted fw-light">/mo</small></h1>
-          <ul class="list-unstyled mt-3 mb-4">
-            <li>10 users included</li>
-            <li>2 GB of storage</li>
-            <li>Email support</li>
-            <li>Help center access</li>
-          </ul>
-          <button type="button" class="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
-        </div>
-      </div>
+  <section id="main">
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+       <div class="carousel-inner">
+         <div class="carousel-item active">
+           <img src="img/slider/img_1.jpg" class="d-block w-100 disabled">
+         </div>
+         <div class="carousel-item">
+           <img src="img/slider/img_2.jpg" class="d-block w-100 disabled">
+         </div>
+         <div class="carousel-item">
+           <img src="img/slider/img_3.jpg" class="d-block w-100 disabled">
+         </div>
+       </div>
+     </div>
+     <!-- Carousel end -->
+     <!--  -->
+     <div class="main-text">
+       <div class="col-md-12 text-center">
+           <h1 class="sliderTextBig text-white greenUnderline fw-bold">
+             Welcome to my test project!
+           </h1>
+           <h3 class="sliderTextSmall text-white mb-3">
+             If you want to try all the permissions with administrator rights, register and let me take it, I will issue them to you, or use:<br><br>
+             <strong>E-Mail: test@gmail.com</strong><br>
+             <strong>Password: 12345</strong>
+           </h3>
+       </div>
     </div>
-    <div class="col">
-      <div class="card mb-4 rounded-3 shadow-sm">
-        <div class="card-header py-3">
-          <h4 class="my-0 fw-normal">Pro</h4>
-        </div>
-        <div class="card-body">
-          <h1 class="card-title pricing-card-title">$15<small class="text-muted fw-light">/mo</small></h1>
-          <ul class="list-unstyled mt-3 mb-4">
-            <li>20 users included</li>
-            <li>10 GB of storage</li>
-            <li>Priority email support</li>
-            <li>Help center access</li>
-          </ul>
-          <button type="button" class="w-100 btn btn-lg btn-primary">Get started</button>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="card mb-4 rounded-3 shadow-sm border-primary">
-        <div class="card-header py-3 text-white bg-primary border-primary">
-          <h4 class="my-0 fw-normal">Enterprise</h4>
-        </div>
-        <div class="card-body">
-          <h1 class="card-title pricing-card-title">$29<small class="text-muted fw-light">/mo</small></h1>
-          <ul class="list-unstyled mt-3 mb-4">
-            <li>30 users included</li>
-            <li>15 GB of storage</li>
-            <li>Phone and email support</li>
-            <li>Help center access</li>
-          </ul>
-          <button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <h2 class="display-6 text-center mb-4">Compare plans</h2>
-
-  <div class="table-responsive">
-    <table class="table text-center">
-      <thead>
-        <tr>
-          <th style="width: 34%;"></th>
-          <th style="width: 22%;">Free</th>
-          <th style="width: 22%;">Pro</th>
-          <th style="width: 22%;">Enterprise</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row" class="text-start">Public</th>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"></use></svg></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"></use></svg></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"></use></svg></td>
-        </tr>
-        <tr>
-          <th scope="row" class="text-start">Private</th>
-          <td></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"></use></svg></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"></use></svg></td>
-        </tr>
-      </tbody>
-
-      <tbody>
-        <tr>
-          <th scope="row" class="text-start">Permissions</th>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"></use></svg></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"></use></svg></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"></use></svg></td>
-        </tr>
-        <tr>
-          <th scope="row" class="text-start">Sharing</th>
-          <td></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"></use></svg></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"></use></svg></td>
-        </tr>
-        <tr>
-          <th scope="row" class="text-start">Unlimited members</th>
-          <td></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"></use></svg></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"></use></svg></td>
-        </tr>
-        <tr>
-          <th scope="row" class="text-start">Extra security</th>
-          <td></td>
-          <td></td>
-          <td><svg class="bi" width="24" height="24"><use xlink:href="#check"></use></svg></td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</main>
-
-</div>
+     <!--  -->
+  </section>
   <?php require "includes/footer.php" ?>
 
 <script src="js/bootstrap.min.js"></script>
