840cfb4f4f1572b7764e9e0302c8d95a7397f6dd
O <oly.queen73@gmail.com>
2022-06-27 11:31:28 +0400

    Added removal of data change notification

--- js/main.js
+++ js/main.js
@@ -8,3 +8,10 @@ if(document.getElementById('logout_btn')){
     document.getElementById('logout_form').submit();
   };
 };
+(function(){
+  if(document.querySelector('.alert')){
+      setTimeout(function(){
+         document.querySelector('.alert').remove();
+      }, 3000);
+  }
+})();
